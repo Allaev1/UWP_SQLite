5daa9a076c0e4568c89e1097fc60a2369e055fce
Bekzod Allaev <Allaev_@outlook.com>
2018-11-01 00:17:59 +0500

    Added logic of AddEditPageViewModel

--- UWP_SQLite_CRUD_Sample/ViewModels/AddEditPageViewModel.cs
+++ UWP_SQLite_CRUD_Sample/ViewModels/AddEditPageViewModel.cs
@@ -4,11 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Template10.Mvvm;
+using DataBase;
+using UWP_SQLite_CRUD_Sample.Views;
 
 namespace UWP_SQLite_CRUD_Sample.ViewModels
 {
-    public class AddEditPageViewModel : ViewModelBase 
+    public class AddEditPageViewModel : ViewModelBase
     {
+        DelegateCommand _saveCommand;
+        NorthwindContext context = new NorthwindContext();
+        public AddEditPageViewModel()
+        {
+            _saveCommand = new DelegateCommand(SaveExecute);
+        }
+
+        //TODO: Rename propertie
+        public Product Product { set; get; }
+
+        #region Commands
+        #region SaveCommand
+        public DelegateCommand Save
+        {
+            get { return _saveCommand ?? (_saveCommand = new DelegateCommand(SaveExecute)); }
+        }
+
+        private void SaveExecute()
+        {
+            context.Products.Add(Product);
+            context.SaveChanges();
+            NavigationService.Navigate(typeof(MainPage));
+        }
+        #endregion
+        #endregion
 
     }
 }
