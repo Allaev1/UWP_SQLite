ed385db42811164be9d5a32d3e1aa249790c26c5
Bekzod Allaev <Allaev_@outlook.com>
2018-11-01 00:06:18 +0500

    Created AddEditPage and its viewmodel and realised navigation to this page

--- UWP_SQLite_CRUD_Sample/ViewModels/MainPageViewModel.cs
+++ UWP_SQLite_CRUD_Sample/ViewModels/MainPageViewModel.cs
@@ -4,20 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Template10.Mvvm;
+using Template10.Common;
+using Template10.Services.NavigationService;
 using DataBase;
 using System.Collections.ObjectModel;
+using UWP_SQLite_CRUD_Sample.Views;
 
 namespace UWP_SQLite_CRUD_Sample.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
         NorthwindContext context = new NorthwindContext();
+        DelegateCommand _addCommand;
+        ObservableCollection<Product> _products;
+        INavigationService navigationService;
+
+        public MainPageViewModel()
+        {
+            _addCommand = new DelegateCommand(AddExecute);
+            _products = new ObservableCollection<Product>(context.Products);
+            navigationService = WindowWrapper.Current().NavigationServices.FirstOrDefault();
+            //Creating instance of NavigationService in 26 line
+            //Otherwise it will be null
+            //TODO: Find out why it happens
+        }
 
         #region Bindable proprties
         public ObservableCollection<Product> Products
         {
-            get { return new ObservableCollection<Product>(context.Products); }
+            set { Set(ref _products, value); }
+            get { return _products; }
         }
+        #endregion
+
+        #region Commands
+        #region AddCommand
+        public DelegateCommand AddCommand
+        {
+            get { return _addCommand ?? (_addCommand = new DelegateCommand(AddExecute)); }
+        }
+
+        private void AddExecute() =>
+            navigationService.Navigate(typeof(AddEditPage));
+
+        #endregion
         #endregion
     }
 }
